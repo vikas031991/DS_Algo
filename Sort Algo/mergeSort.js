562e005984e7d3fe359ce655343a1a60d6254826
Vikas Telkar <vikas147telkar@gmail.com>
2023-03-06 17:54:49 +0530

    Create mergeSort.js

---
+++ Sort Algo/mergeSort.js
@@ -0,0 +1,24 @@
+function mergeSort(arr) {
+  if(arr.length < 2) {
+    return arr;
+  }
+  let mid = Math.floor(arr.length/2);
+  let leftArr = arr.slice(0, mid);
+  let rightArr = arr.slice(mid);
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+function merge(leftArr, rightArr) {
+  let sortedArr = [];
+  while (leftArr.length && rightArr.length) {
+    if(leftArr[0] <= rightArr[0]) {
+      sortedArr.push(leftArr.shift());
+    } else {
+      sortedArr.push(rightArr.shift());
+    }
+  }
+  return [...sortedArr, ...leftArr, ...rightArr];
+}
+
+console.log(mergeSort([-6, 20, 8, -2, 4]));
+console.log(mergeSort([8, 20, -2, 4, -6]));
